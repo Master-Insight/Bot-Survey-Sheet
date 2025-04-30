aaa7870bffc625d7115a0dfb3874bae8bb78ff02
Gustavo Sirtori <gustavo.sirtori@gmail.com>
2025-04-30 16:24:34 -0300

    👷 feat (config): handlebars

--- app/cmd/server.js
+++ app/cmd/server.js
@@ -1,7 +1,9 @@
 import express from "express";
+import handlebars from 'express-handlebars';
 import configEnv from "../config/env.js";
 import appRouter from '../modules/routes.js'
 import handleResponses from "../pkg/middleware/handleResponses.js";
+import __dirname from "../pkg/utils/dirname.js";
 
 // App initialization ------------------------------
 const app = express();
@@ -9,10 +11,16 @@ const app = express();
 // App Configurations --------------------------------
 const port = configEnv.PORT || 8080;
 app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// Handlebars --------------------------------
+app.engine('hbs', handlebars.engine({ extname: '.hbs' }));
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/app/pages');
 
 // App Middleware --------------------------------
 app.use(handleResponses)
-
+handlebars
 // App Routes --------------------------------
 app.use('/', appRouter);
 
