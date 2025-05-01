1f105af6f030cdff8a4b09538c5c7cc838b9c227
Gustavo Sirtori <gustavo.sirtori@gmail.com>
2025-05-01 18:58:29 -0300

    🔼 update (Pages): restructure

--- app/modules/views.routes.js
+++ app/modules/views.routes.js
@@ -3,14 +3,14 @@ import { Router } from "express";
 const router = Router()
 
 router
-  .get("/prueba", (req, res) => {
-    res.renderPage("index", "Prueba", {})
-  })
   .get("/", (req, res) => {
-    res.send(`
-      <h3>Server online</h3>
-      <pre>Nothing to see here.
-  Checkout README.md to start.</pre>`);
+    res.renderPage("index", "Home", {})
+  })
+  .get("/politica", (req, res) => {
+    res.renderPage("politica", "Politica", {})
+  })
+  .get("/conducta", (req, res) => {
+    res.renderPage("conducta", "Conducta", {})
   })
 
 export default router
