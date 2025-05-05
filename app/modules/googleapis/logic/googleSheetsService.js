7a16d0db7a619acc30bbfacb41d9fcfb428eac55
Gustavo Sirtori <gustavo.sirtori@gmail.com>
2025-05-04 21:10:24 -0300

    🐾 update (googleSheetsService): updateSheetCell

--- app/modules/googleapis/logic/googleSheetsService.js
+++ app/modules/googleapis/logic/googleSheetsService.js
@@ -17,14 +17,14 @@ const auth = new google.auth.JWT(
 const sheets = google.sheets({ version: 'v4', auth })
 
 /**
- * ✍️ Escribe una fila en la hoja o rango indicado
+ * ✍️ Escribe una fila en la hoja o rango indicado (modo append)
  * @param {Array} data - Array de datos (ej: ['Juan', 'Pérez', 'juan@mail.com'])
  * @param {string} range - Nombre de hoja (ej: 'answers'), rango (ej: 'Consultas!A:D') o rango nombrado (ej: 'TPREGUNTAS')
  * @param {string} spreadsheetId - Opcional, usa el ID por defecto si no se pasa
  */
 export async function addToSheet(data, range = 'answers', spreadsheetId = GOOGLE_SHEETS_ID) {
   try {
-    const response = await sheets.spreadsheets.values.append({
+    await sheets.spreadsheets.values.append({
       spreadsheetId,
       range,
       valueInputOption: 'RAW',
@@ -42,7 +42,7 @@ export async function addToSheet(data, range = 'answers', spreadsheetId = GOOGLE
 }
 
 /**
- * 📖 Lee datos desde un rango (ej: 'Consultas!A:G' o un rango nombrado como 'TPREGUNTAS')
+ * 📖 Lee datos desde un rango (ej: 'Consultas!A:G' o un rango nombrado como 'TPREGUNTAS') (modo get)
  * @param {string} range - Rango explícito o rango nombrado (debe existir en el archivo)
  * @param {string} spreadsheetId - Opcional, usa el ID por defecto si no se pasa
  * @returns {Array[]} - Array de arrays de valores
@@ -56,6 +56,29 @@ export async function getFromSheet(range = "TPREGUNTAS", spreadsheetId = GOOGLE_
     return response.data.values
   } catch (error) {
     console.error('❌ Error en readRange::', error);
+    throw error
+  }
+}
+
+/**
+ * 🔁 Actualiza una celda específica (por fila y columna)
+ * @param {any} value - Valor a colocar
+ * @param {String} cell - Celda (ej: 'Hoja1!C4')
+ * @param {string} spreadsheetId - Opcional, usa el ID por defecto si no se pasa
+ */
+export async function updateSheetCell(value, cell, spreadsheetId = GOOGLE_SHEETS_ID) {
+  try {
+    await sheets.spreadsheets.values.update({
+      spreadsheetId,
+      range: cell,
+      valueInputOption: 'RAW',
+      resource: {
+        values: [[value]]
+      },
+    })
+    return `✅ Celda ${cell} actualizada`
+  } catch (error) {
+    console.error(`❌ Error en updateSheetCell ${cell}:`, error)
     throw error
   }
 }
