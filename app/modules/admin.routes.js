15f6a0254affa8a009b3b34a836e738da6bc5d76
Gustavo Sirtori <gustavo.sirtori@gmail.com>
2025-05-11 18:42:59 -0300

    🔼 update (Routes): admin

---
+++ app/modules/admin.routes.js
@@ -0,0 +1,26 @@
+import { Router } from "express";
+import { authenticate } from "./auth.routes.js";
+import { getFromSheet } from "./googleapis/logic/googleSheetsService.js";
+import configEnv from "../config/env.js";
+
+const router = Router();
+const { CONFIG_SHEET } = configEnv
+
+router.use(authenticate); // Todas las rutas aquí requieren autenticación
+
+router.get('/admin', async (req, res) => {
+  try {
+    const config = await getFromSheet(CONFIG_SHEET);
+    const surveys = Array.isArray(config) ? config.slice(1).map(rgln => rgln[0]) : [];
+
+    res.renderPage("admin", "Panel Admin", {
+      admin: true,
+      surveys
+    });
+  } catch (error) {
+    console.error('Error en panel admin:', error);
+    res.status(500).send('Error al cargar el panel');
+  }
+});
+
+export default router;
